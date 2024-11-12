91024b56c0d2c56c2e7b2053e9f83eee31e8db94
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-13 00:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let message = document.querySelector(".message");
 let boxes = document.querySelectorAll(".box");
 let player1 = document.getElementById("player-1");
 let player2 = document.getElementById("player-2");
-let startGame = document.getElementById("submit")
+let startGame = document.getElementById("submit");
 let playersName = document.querySelector(".playersName");
 let boxContainer = document.querySelector(".box_container");
 
@@ -17,7 +17,6 @@ let patterns = [
   [2, 1, 0],
   [2, 5, 8],
   [2, 4, 6],
-  [2, 4, 5],
   [5, 4, 3],
   [6, 7, 8],
   [6, 3, 0],
@@ -31,19 +30,14 @@ let patterns = [
 // Show the turn of player according to given the values of x or y.
 let turn = true;
 
-// Declare player name globally so that specifically player name could be render anywhere.
-let player1Name
-let player2Name
-
 // Click button to start the game by player name
 startGame.addEventListener("click", () => {
   console.log(player1.value + " " + player2.value);
-  player1Name = player1.value;
-  player2Name = player2.value;
 
-  message.firstElementChild.innerHTML = `${player1Name}, you're up`;
-  message.lastElementChild.innerHTML = `${player2Name}, you're up`;
-  message.lastElementChild.style.display = "none";
+  //   message.firstElementChild.innerHTML = `${player1Name}, you're up`;
+  //   message.lastElementChild.innerHTML = `${player2Name}, you're up`;
+  //   message.lastElementChild.style.display = "none";
+  message.innerHTML = player1.value;
 
   if (player1.value != "" && player2.value != "") {
     playersName.style.display = "none";
@@ -51,20 +45,24 @@ startGame.addEventListener("click", () => {
   } else {
     alert("Please fill player name");
   }
-
 });
 
 // Show the turn of player name.
 const playerTurn = (turn) => {
+  //   if (turn) {
+  //     message.firstElementChild.style.display = "block";
+  //     message.lastElementChild.style.display = "none";
+  //   } else {
+  //     message.firstElementChild.style.display = "none";
+  //     message.lastElementChild.style.display = "block";
+  //   }
+
   if (turn) {
-    message.firstElementChild.style.display = "block";
-    message.lastElementChild.style.display = "none";
+    message.innerHTML = player1.value;
   } else {
-    message.firstElementChild.style.display = "none";
-    message.lastElementChild.style.display = "block";
+    message.innerHTML = player2.value;
   }
 };
-
 
 // Match the pattern to make winner between player1 and player2.
 const winner = () => {
@@ -83,9 +81,11 @@ const winner = () => {
           box.disabled = true;
 
           if (pattern1.innerText == "x") {
-            message.firstElementChild.innerText = `${player1Name}, congratulations you won!`;
+            // message.firstElementChild.innerText = `${player1Name} congratulations you won!`;
+            message.innerHTML = player1.value + "winner";
           } else {
-            message.lastElementChild.innerText = `${player2Name}, congratulations you won!`;
+            // message.lastElementChild.innerText = `${player2Name} congratulations you won!`;
+            message.innerHTML = player2.value + "winner";
           }
 
           pattern1.style.color = "green";
@@ -101,6 +101,33 @@ const winner = () => {
   else return false;
 };
 
+const disableOnMatchDraw = () => {
+  let count = 0;
+
+  boxes.forEach((box) => {
+    if (box.innerText != "") {
+      count++;
+    }
+  });
+
+  console.log(count, boxes.length);
+  if (count == boxes.length) {
+    boxes.forEach((box) => {
+      box.disabled = true;
+      box.style.opacity = ".7";
+      //   message.firstElementChild.style.display = "none";
+      //   message.lastElementChild.style.display = "none";
+
+      //   let span = document.createElement("span");
+      //   span.innerText = "Match Draw";
+      //   message.appendChild(span);
+
+      message.innerHTML = "Match draw";
+      return false;
+    });
+  }
+  return true;
+};
 
 // Main root where every box is unpacked one by one.
 boxes.forEach((box) => {
@@ -114,9 +141,11 @@ boxes.forEach((box) => {
       turn = true;
     }
 
+    let boxFull = disableOnMatchDraw();
+
     let win = winner();
     console.log(win);
 
-    if (win == false) playerTurn(turn);
+    if (win == false && boxFull == false) playerTurn(turn);
   });
 });
